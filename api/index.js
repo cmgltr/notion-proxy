7266e80044c19ad653ac748376e2c0f77b3bacf5
cmgltr <engcmgl@gmail.com>
2025-05-23 14:40:15 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-  res.writeHead(302, { Location: "https://mecsys.notion.site/Mechanical-Systems-Forum-..." });
+  res.writeHead(302, { Location: "https://mecsys.notion.site/Mechanical-Systems-Forum-1ddd6db7d2508011b79cf032297d399f?pvs=4" });
   res.end();
 }
