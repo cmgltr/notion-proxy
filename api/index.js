d2a33c04c8905285a5a5a7e2b666dac647cb375c
cmgltr <engcmgl@gmail.com>
2025-05-23 13:43:28 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 export default async function handler(req, res) {
   const notionUrl = "https://mecsys.notion.site/Mechanical-Systems-Forum-1ddd6db7d2508011b79cf032297d399f";
-  res.writeHead(302, { Location: notionUrl });
-  res.end();
+  try {
+    const response = await fetch(notionUrl);
+    const html = await response.text();
+    res.setHeader("Content-Type", "text/html");
+    res.send(html);
+  } catch (e) {
+    res.status(500).send("Notion sayfası yüklenemedi.");
+  }
 }
